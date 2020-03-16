288524cb2a735e3e34ab07e31b5aaceb9fd3364d
Namit Doshi <namitdoshi90@gmail.com>
2020-03-16 23:40:17 +0530

    added path of holder component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component'
+import { ViewAddScreensComponent } from './view-add-screens/view-add-screens.component'
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent }
+  {path: 'dashboard', component: DashboardComponent },
+  {path: 'add-expense-income', component: ViewAddScreensComponent}
 ];
 
 @NgModule({
