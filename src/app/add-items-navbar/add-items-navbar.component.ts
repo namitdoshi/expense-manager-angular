831cbc378499f8c32912d271f13ced1f3b018957
Namit Doshi <namitdoshi90@gmail.com>
2020-03-17 00:09:23 +0530

    added a drop downn for income and expense

--- src/app/add-items-navbar/add-items-navbar.component.ts
+++ src/app/add-items-navbar/add-items-navbar.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Selector {
+  value: string;
+}
+
 @Component({
   selector: 'app-add-items-navbar',
   templateUrl: './add-items-navbar.component.html',
   styleUrls: ['./add-items-navbar.component.scss']
 })
 export class AddItemsNavbarComponent implements OnInit {
+
+  select: Selector[] = [
+    {value: 'Expenses'},
+    {value: 'Income'}
+  ]
 
   constructor() { }
 
