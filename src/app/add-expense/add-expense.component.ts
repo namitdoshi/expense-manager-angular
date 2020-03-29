a6d02a902c49c09e3f9c6788ee906daedd881bc9
Namit Doshi <namitdoshi90@gmail.com>
2020-03-30 01:03:51 +0530

    expense component tiles created and styled

--- src/app/add-expense/add-expense.component.ts
+++ src/app/add-expense/add-expense.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+  interface expenseArray { 
+    icon: string,
+    name: string 
+  }
+
 @Component({
   selector: 'app-add-expense',
   templateUrl: './add-expense.component.html',
   styleUrls: ['./add-expense.component.scss']
 })
 export class AddExpenseComponent implements OnInit {
+
+  expenses: expenseArray[] = [
+    { icon: 'local_dining', name: 'Food' },
+    { icon: 'directions_bus', name: 'Transportation' },
+    { icon: 'home', name: 'Home' },
+    { icon: 'offline_bolt', name: 'Electricity' },
+    { icon: 'drive_eta', name: 'Car' },
+    { icon: 'videogame_asset', name: 'Entertainment' },
+    { icon: 'shopping_cart', name: 'Shopping' },
+    { icon: '', name: '' },
+    { icon: '', name: '' },
+    { icon: '', name: '' },
+    // { icon: '', name: '' },
+
+  ]
 
   constructor() { }
 
