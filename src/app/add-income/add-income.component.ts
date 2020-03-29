219423268700b80a95bc337ae766244cce98a819
Namit Doshi <namitdoshi90@gmail.com>
2020-03-30 00:26:07 +0530

    used ngfor to display chips

--- src/app/add-income/add-income.component.ts
+++ src/app/add-income/add-income.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface expenseArray {
+  icon: string,
+  name: string
+}
+
 @Component({
   selector: 'app-add-income',
   templateUrl: './add-income.component.html',
   styleUrls: ['./add-income.component.scss']
 })
 export class AddIncomeComponent implements OnInit {
+
+  expenses: expenseArray[] = [
+    { icon: 'account_balance_wallet', name: 'Salary'},
+    { icon: 'card_giftcard', name: 'Gifts'},
+    // { icon: '', name: ''}
+  ]
 
   constructor() { }
 
